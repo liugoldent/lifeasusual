9d0449d33d5cedb992cc93769c19136fed8171a8
liugoldent <liugoldent@gmail.com>
2021-08-18 15:15:41 +0800

    first commit

---
+++ lifeasusual.md
@@ -0,0 +1,4 @@
+* 網頁應用程式
+* 網址：https://script.google.com/macros/s/AKfycbz8Cq0NYJYtEzzfrooLboCao06W4evkLYxn1u5S7ZPny6sKtS_S5kUC424n3KDBswM8/exec
+* 部署ID
+*ID:AKfycbz8Cq0NYJYtEzzfrooLboCao06W4evkLYxn1u5S7ZPny6sKtS_S5kUC424n3KDBswM8
---
+++ src/views/Order.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    <h1>This is an Order page</h1>
+    <button @click="send()">Click</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Order",
+  components: {},
+  methods: {
+    send() {
+      console.log("send");
+
+      this.axios
+        .post(
+          "https://script.google.com/macros/s/AKfycbytQm3cq7KYye607UbMzQzscAZFe0bvOvTEDlmu2JK8f1nh9anpIxcWaiFNPMLuniQbcw/exec",
+          {
+            method: "write",
+            name: "Wayne",
+            sex: "male",
+            remark: "測試寫入功能",
+          }
+        )
+        .then(function(response) {
+          console.log("1");
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log("2");
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
